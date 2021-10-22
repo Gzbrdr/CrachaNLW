6cf2ecc63caa719ecf8370d9d612aba32948d2e7
Gabriel Zanella <92189638+Gzbrdr@users.noreply.github.com>
2021-10-22 01:13:38 -0300

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,30 @@
+const linksSocialMedia = {
+  github: 'gzbrdr',
+  youtube: '/channel/UCyAlel5JZwaL8S79APFqLMw',
+  instagram: 'gabrielzbardini',
+  twitter: 'gzbrdr'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      username.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userLogin.textContent = data.login
+      userImage.src = data.avatar_url
+      userImage.alt = `Foto de ${data.name}`
+    })
+}
+getGitHubProfileInfos()

85703545f247afa702f9fb56eda204046baab45d
Gabriel Zanella <92189638+Gzbrdr@users.noreply.github.com>
2021-10-22 01:46:44 -0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,6 @@ const linksSocialMedia = {
   instagram: 'gabrielzbardini',
   twitter: 'gzbrdr'
 }
-
 function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
